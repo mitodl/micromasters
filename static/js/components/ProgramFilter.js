492095894ed18494be733e7bdf336eb520a85a76
George Schneeloch <gschneel@mit.edu>
2016-09-08 11:35:46 -0400

    Flow

--- static/js/components/ProgramFilter.js
+++ static/js/components/ProgramFilter.js
@@ -9,7 +9,13 @@ import {
 } from 'searchkit';
 import _ from 'lodash';
 
+import type { ProgramEnrollment } from '../flow/enrollmentTypes';
+
 export default class ProgramFilter extends SearchkitComponent {
+  props: {
+    currentProgramEnrollment: ProgramEnrollment,
+  };
+
   _accessor = new AnonymousAccessor(query => {
     const { currentProgramEnrollment } = this.props;
     if (currentProgramEnrollment === null) {
@@ -23,8 +29,7 @@ export default class ProgramFilter extends SearchkitComponent {
     return this._accessor;
   }
 
-  componentDidUpdate(...args) {
-    const [prevProps] = args;
+  componentDidUpdate(prevProps: Object): void {
     if (!_.isEqual(prevProps.currentProgramEnrollment, this.props.currentProgramEnrollment)) {
       // (╯°□°)╯︵ ┻━┻
       this.context.searchkit.reloadSearch();
