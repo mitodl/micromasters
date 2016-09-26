ba7c5f155fdee272d61108184806664feda2fc3d
David Baumgold <david@davidbaumgold.com>
2016-09-26 15:03:34 -0400

    Embiggen carousel pictures

--- static/js/components/FacultyCarousel.js
+++ static/js/components/FacultyCarousel.js
@@ -47,6 +47,12 @@ export default class FacultyCarousel extends React.Component {
       slidesToShow: 2.2,
       slidesToScroll: 1,
       adaptiveHeight: false,
+      responsive: [{
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1.5,
+        }
+      }]
     };
     const tiles = this.props.faculty.map((faculty, index) =>
       // react-slick only works with <div>s, not React components,
