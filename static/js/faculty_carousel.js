edef599740423656f5868453b5794237cfeb60dd
David Baumgold <david@davidbaumgold.com>
2016-09-26 09:40:00 -0400

    Use Array.from().map, empty carouselDiv

--- static/js/faculty_carousel.js
+++ static/js/faculty_carousel.js
@@ -4,17 +4,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// define a generic `map` function, that can be used on a NodeList
-let map = (...fnargs) => {
-  let args = [].slice.call(fnargs, 0);
-  let ctx = args.shift();
-  return [].map.apply(ctx, args);
-};
-
-
 // map over the <li> nodes to get faculty information
 const carouselDiv = document.querySelector('#faculty-carousel');
-let faculty = map(carouselDiv.querySelectorAll("li"), (li) => {
+
+let faculty = Array.from(carouselDiv.querySelectorAll("li")).map((li) => {
   let data = {};
   let imgEl = li.querySelector("img.faculty-photo");
   if (imgEl) {
@@ -29,6 +22,10 @@ let faculty = map(carouselDiv.querySelectorAll("li"), (li) => {
   return data;
 });
 
+// empty the carouselDiv
+while(carouselDiv.hasChildNodes()) {
+  carouselDiv.removeChild(carouselDiv.lastChild);
+}
 
 // pass faculty information to the <FacultyCarousel> component
 ReactDOM.render(
