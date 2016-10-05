7129e4ab91276eac53c2b53a245c85c374baf7cb
Alvin Siu <alvin@zagaran.com>
2016-10-05 16:55:17 -0400

    Fixed course price API output

--- financialaid/api.py
+++ financialaid/api.py
@@ -84,14 +84,17 @@ def get_no_discount_tier_program(program_id):
 def get_formatted_course_price(program_enrollment):
     """
     Returns dictionary of information about the course price for a learner.
+    
+    Note: "price" will always include discounts from financial aid applications, even if the
+    application's status is not yet approved.
 
     Args:
         program_enrollment (ProgramEnrollment): program enrollment record for the learner
             whose price we're retrieving
     Returns:
         dict: {
-            "price": float - the course price
-            "financial_aid_adjustment": bool - if financial aid is approved and has been applied to this course price,
+            "program_id": int - the Program's id
+            "price": float - the course price minus any discounts (whether or not it's approved)
             "financial_aid_availability": bool - Program.financial_aid_availability,
             "has_financial_aid_request": bool - if has a financial aid request
         }
@@ -100,7 +103,6 @@ def get_formatted_course_price(program_enrollment):
     program = program_enrollment.program
 
     has_financial_aid_request = False
-    financial_aid_adjustment = False
     financial_aid_availability = False
     course_price = program.get_course_price()
 
@@ -115,14 +117,10 @@ def get_formatted_course_price(program_enrollment):
             has_financial_aid_request = True
             # FinancialAid.save() only allows one object per (user, tier_program__program) pair
             financial_aid = financial_aid_queryset.first()
-            if financial_aid.status == FinancialAidStatus.APPROVED:
-                # If the financial aid request is approved, adjust course price
-                course_price = course_price - financial_aid.tier_program.discount_amount
-                financial_aid_adjustment = True
+            course_price = course_price - financial_aid.tier_program.discount_amount
     return {
         "program_id": program.id,
         "price": course_price,
-        "financial_aid_adjustment": financial_aid_adjustment,
         "financial_aid_availability": financial_aid_availability,
         "has_financial_aid_request": has_financial_aid_request
     }
