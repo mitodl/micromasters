9affdb7397e0b9a68b0f2f6ef6277365f60f1f92
Miriam Farkas <miriam@zagaran.com>
2016-10-06 11:34:01 -0400

    fixes to test

--- financialaid/admin_test.py
+++ financialaid/admin_test.py
@@ -3,34 +3,24 @@ Tests for admin.py
 """
 from unittest.mock import Mock
 
-from rest_framework.test import APIClient
-
 from financialaid.admin import FinancialAidAdmin
 from financialaid.api_test import FinancialAidBaseTestCase
 from financialaid.models import FinancialAidAudit
 
 
-class AdminTest(FinancialAidBaseTestCase, APIClient):
+class AdminTest(FinancialAidBaseTestCase):
     """
     Tests specifically whether new FinancialAidAudit object is created when the financial aid
     admin model is changed.
     """
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-
-    def setUp(self):
-        super(AdminTest, self).setUp()
-
     def test_save_and_log_model(self):
         """
-        Tests that save_model() function on FinancialAidAdmin admin model creates FinancialAidAudit
+        Tests that the save_model() function on FinancialAidAdmin model creates FinancialAidAudit
         object
         """
         assert FinancialAidAudit.objects.count() == 0
         financial_aid_admin = FinancialAidAdmin(model=self.financialaid_pending, admin_site=Mock())
-        mock_request = Mock()
-        mock_request.user = self.staff_user_profile.user
+        mock_request = Mock(user=self.staff_user_profile.user)
         financial_aid_admin.save_model(
             request=mock_request,
             obj=financial_aid_admin.model,
