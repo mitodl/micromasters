d1db83031d6aad37b5a2f8b9a754acd8f3f30da9
George Schneeloch <gschneel@mit.edu>
2016-10-06 14:56:29 -0400

    Flow

--- static/js/components/dashboard/FinancialAidCard.js
+++ static/js/components/dashboard/FinancialAidCard.js
@@ -38,7 +38,6 @@ export default class FinancialAidCard extends React.Component {
     documents:                      DocumentsState,
     setDocumentSentDate:            (sentDate: string) => void,
     updateDocumentSentDate:         (financialAidId: number, sentDate: string) => Promise<*>,
-    fetchDashboard:                 () => void,
     setConfirmSkipDialogVisibility: (b: boolean) => void,
     ui:                             UIState,
     skipFinancialAid:               () => void,
