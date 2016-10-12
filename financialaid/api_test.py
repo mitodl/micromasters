6aa98b5ecaaf6563397e8cec161df5f5c87264f6
Alvin Siu <alvin@zagaran.com>
2016-10-12 11:01:59 -0400

    Removed explicit set to "US" country code

--- financialaid/api_test.py
+++ financialaid/api_test.py
@@ -41,9 +41,7 @@ class FinancialAidBaseTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         with mute_signals(post_save):
-            cls.profile = ProfileFactory.create(
-                country="US"
-            )
+            cls.profile = ProfileFactory.create()
             cls.profile2 = ProfileFactory.create()
             cls.staff_user_profile = ProfileFactory.create()
             cls.staff_user_profile2 = ProfileFactory.create()
