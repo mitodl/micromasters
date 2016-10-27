5323d84325f682ac4f0ae7696b6e13012eb127c7
George Schneeloch <gschneel@mit.edu>
2016-10-27 16:13:52 -0400

    Handle non-zero error code for makemigrations

--- detect_missing_migrations.sh
+++ detect_missing_migrations.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 TMPFILE=$(mktemp)
-./manage.py makemigrations --no-input --dry-run >& "$TMPFILE"
-if cat "$TMPFILE" | grep -v "No changes detected" > /dev/null
-then
-    echo "Error: one or more migrations are missing:"
+fail() {
+    echo "Error: one or more migrations are missing"
     echo
     cat "$TMPFILE"
     rm "$TMPFILE"
     exit 1
+}
+
+./manage.py makemigrations --no-input --dry-run >& "$TMPFILE"
+if [[ $? ]]
+then
+    # makemigrations has returned a non-zero for some reason, possibly
+    # because it needs input but --no-input is set
+    fail;
+elif cat "$TMPFILE" | grep -v "No changes detected" > /dev/null
+then
+    fail;
 else
     rm "$TMPFILE"
 fi
