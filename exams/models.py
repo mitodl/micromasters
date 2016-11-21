2f6a78bd122096e389d640202139710d6b2cec95
Nathan Levesque <nlevesq@mit.edu>
2016-11-21 16:13:47 -0500

    Added models for Pearson proctored exams

---
+++ exams/models.py
@@ -0,0 +1,75 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator
+
+
+import uuid
+
+
+class ProctoredProfile(models.Model):
+    user = models.ForeignKey(User, null=False, unique=True)
+
+    updated_on = models.DateTimeField(auto_now=True)
+    synced_on = models.DateTimeField(blank=True, null=True)
+
+    first_name = models.TextField(
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    last_name = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True
+    )
+
+    address1 = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True
+    )
+    address2 = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True
+    )
+    address3 = models.CharField(
+        max_length=40,
+        blank=True,
+        null=True
+    )
+
+    country = models.CharField(
+        max_length=3,
+        blank=True,
+        null=True
+    )
+    state_or_territory = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+    )
+    city = models.CharField(
+        max_length=32,
+        blank=True,
+        null=True
+    )
+    postal_code = models.CharField(
+        max_length=16,
+        blank=True,
+        null=True
+    )
+
+    phone_number = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True
+    )
+    phone_country_code = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+        validators=[
+            MaxValueValidator(999),
+        ]
+    )
