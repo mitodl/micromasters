765df1afae3a45f719394f5c0b465e518ce4ef15
Nathan Levesque <nlevesq@mit.edu>
2016-11-23 16:13:09 -0500

    Fix indent

--- exams/utils.py
+++ exams/utils.py
@@ -7,5 +7,5 @@ def strip_non_cp_1252_chars(source):
     Returns a new string with characters outside the CP-1252 charset stripped
     """
     return source \
-            .encode('cp1252', 'ignore') \
-            .decode('utf-8')
+        .encode('cp1252', 'ignore') \
+        .decode('utf-8')
