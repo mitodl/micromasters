5b0647a43daf6518e94bc1c953935d9f7913f2e0
George Schneeloch <gschneel@mit.edu>
2016-12-14 10:50:05 -0500

    Made function for dialog actions

--- static/js/util/util.js
+++ static/js/util/util.js
@@ -9,7 +9,9 @@ import iso3166 from 'iso-3166-2';
 import { S } from '../lib/sanctuary';
 const { Maybe, Just, Nothing } = S;
 import R from 'ramda';
+import Button from 'react-mdl/lib/Button';
 
+import SpinnerButton from '../components/SpinnerButton';
 import {
   STATUS_PASSED,
   EDUCATION_LEVELS,
@@ -458,3 +460,27 @@ export function getUserDisplayName(profile: Profile): string {
 
   return `${first} ${last}${preferred_name}`;
 }
+
+/**
+ * Helper function to create dialog action buttons, with a SpinnerButton for the save button
+ */
+export const dialogActions = (
+  onCancel: Function, onSave: Function, inFlight: bool, text: string='Save', saveClass: string=''
+) => ([
+  <Button
+    type='cancel'
+    key='cancel'
+    className="secondary-button cancel-button"
+    onClick={onCancel}>
+    Cancel
+  </Button>,
+  <SpinnerButton
+    component={Button}
+    spinning={inFlight}
+    type='button'
+    key='save'
+    className={`primary-button save-button ${saveClass}`}
+    onClick={onSave}>
+    {text}
+  </SpinnerButton>
+]);
