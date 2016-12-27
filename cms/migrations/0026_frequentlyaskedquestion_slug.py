ce77f963f508a503e00cc16bd676f111e5b993bf
annagav <annagav@mit.edu>
2016-12-27 14:43:34 -0500

    Combine migrations into one file

--- cms/migrations/0026_frequentlyaskedquestion_slug.py
+++ cms/migrations/0026_frequentlyaskedquestion_slug.py
@@ -4,6 +4,23 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+from django.utils.text import slugify
+
+
+def gen_unique_slug(apps, schema_editor):
+    FrequentlyAskedQuestion = apps.get_model('cms', 'FrequentlyAskedQuestion')
+    for row in FrequentlyAskedQuestion.objects.all():
+        if not row.slug:
+            slug = orig_slug = slugify(row.question)
+            slug_is_unique = not FrequentlyAskedQuestion.objects.filter(slug=orig_slug).exists()
+            count = 1
+            while not slug_is_unique:
+                slug = "{orig}-{count}".format(orig=orig_slug, count=count)
+                slug_is_unique = not FrequentlyAskedQuestion.objects.filter(slug=slug).exists()
+                count += 1
+            row.slug = slug
+            row.save()
+
 
 class Migration(migrations.Migration):
 
@@ -12,9 +29,18 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        # First add a new field for slug
         migrations.AddField(
             model_name='frequentlyaskedquestion',
             name='slug',
             field=models.SlugField(default=None, null=True),
+        ),
+        # Then populate existing rows with unique slugs
+        migrations.RunPython(gen_unique_slug, reverse_code=migrations.RunPython.noop),
+        # Now can make this field unique
+        migrations.AlterField(
+            model_name='frequentlyaskedquestion',
+            name='slug',
+            field=models.SlugField(blank=True, default=None, unique=True),
         ),
     ]
