c2c0966bda5440f2ce89bcd40b75c31babfb6fd5
George Schneeloch <gschneel@mit.edu>
2016-12-29 16:08:57 -0500

    Show spinner only on currently active button

--- static/js/components/SpinnerButton.js
+++ static/js/components/SpinnerButton.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import Spinner from 'react-mdl/lib/Spinner';
 
+type SpinnerButtonProps = {
+  spinning: bool,
+  component: React.Component<*, *, *>,
+  className?: string,
+  onClick?: Function,
+  children?: any,
+  disabled?: ?bool,
+};
+
 export default class SpinnerButton extends React.Component {
-  props: {
-    spinning: bool,
-    component: React.Component<*, *, *>,
-    className?: string,
-    onClick?: Function,
-    children?: any,
-    disabled?: ?bool,
-  };
+  props: SpinnerButtonProps;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // This keeps track of if a button was recently clicked, to indicate
+      // that props.spinning is relevant to this button. If a button was not
+      // clicked but spinning=true, it will be ignored.
+      recentlyClicked: false
+    };
+  }
+
+  componentWillReceiveProps(nextProps: SpinnerButtonProps) {
+    if (!nextProps.spinning && this.props.spinning) {
+      // spinning has finished, so reset the state
+      this.setState({
+        recentlyClicked: false
+      });
+    }
+  }
 
   render() {
     let {
@@ -21,17 +42,30 @@ export default class SpinnerButton extends React.Component {
       disabled,
       ...otherProps
     } = this.props;
+    const { recentlyClicked } = this.state;
 
     if (spinning && !disabled) {
-      if (!className) {
-        className = '';
+      if (recentlyClicked) {
+        if (!className) {
+          className = '';
+        }
+        className = `${className} disabled-with-spinner`;
+        children = <Spinner singleColor/>;
       }
-      className = `${className} disabled-with-spinner`;
-      children = <Spinner singleColor />;
       disabled = true;
     }
     if (disabled) {
       onClick = undefined;
+    }
+
+    if (onClick) {
+      let oldOnClick = onClick;
+      onClick = (...args) => {
+        this.setState({
+          recentlyClicked: true
+        });
+        return oldOnClick(...args);
+      };
     }
 
     return <ComponentVariable
