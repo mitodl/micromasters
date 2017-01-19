652f1b05148eb12988a9bfb4c8ed15dd5273a04a
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2017-01-19 22:00:10 +0500

    Refactor code

--- seed_data/utils_test.py
+++ seed_data/utils_test.py
@@ -1,36 +1,27 @@
 """Tests for utils file"""
 import datetime
 from unittest import TestCase
+from ddt import ddt, data, unpack
 
 from seed_data.utils import add_year
 
 
+@ddt
 class AddYearTests(TestCase):
     """Tests for add year method"""
 
-    def test_add_year(self):  # pylint: disable=no-self-use
+    @data(
+        # year, month, day, expected_year, expected_month, expected_day
+        [2016, 2, 29, 2017, 3, 1],
+        [2016, 2, 28, 2017, 2, 28],
+        [2015, 2, 28, 2016, 2, 28]
+    )
+    @unpack
+    def test_add_year(self, year, month, day, expected_year, expected_month, expected_day):
         """assert add year method works"""
-        # assert next year day, month for leap year.
-        leap_year = datetime.datetime(2016, 2, 29, 0, 0, 0)
+        # pylint: disable=no-self-use, too-many-arguments
+        leap_year = datetime.datetime(year, month, day, 0, 0, 0)
         next_year_date = add_year(leap_year, years=1)
-        assert leap_year.year + 1 == next_year_date.year
-        assert leap_year.month + 1 == next_year_date.month
-        assert leap_year.day == 29
-        assert next_year_date.day == 1
-
-    def test_add_year_to_leap_year(self):  # pylint: disable=no-self-use
-        """assert add year method works on leap year"""
-        # assert next year day, month for leap year.
-        leap_year = datetime.datetime(2016, 2, 28, 0, 0, 0)
-        next_year_date = add_year(leap_year, years=1)
-        assert leap_year.year + 1 == next_year_date.year
-        assert leap_year.month == next_year_date.month
-        assert leap_year.day == next_year_date.day
-
-    def test_add_year_when_next_year_leap(self):  # pylint: disable=no-self-use
-        """assert add year method works when next year is leap year"""
-        leap_year = datetime.datetime(2015, 2, 28, 0, 0, 0)
-        next_year_date = add_year(leap_year, years=1)
-        assert leap_year.year + 1 == next_year_date.year
-        assert leap_year.month == next_year_date.month
-        assert leap_year.day == next_year_date.day
+        assert next_year_date.year == expected_year
+        assert next_year_date.month == expected_month
+        assert next_year_date.day == expected_day
