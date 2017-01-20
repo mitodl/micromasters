dbb5f6f04fc6397029bdbe614bb5822bcff22ba6
Nathan Levesque <nlevesq@mit.edu>
2017-01-20 17:32:39 -0500

    Tests for filtered_files

--- exams/pearson/download_test.py
+++ exams/pearson/download_test.py
@@ -1,10 +1,19 @@
 """Pearson SFTP download tests"""
-from unittest.mock import Mock
+import re
+from unittest.mock import (
+    call,
+    Mock,
+)
 
 from django.test import SimpleTestCase, override_settings
 
-from exams.pearson import download
-from exams.pearson.sftp_test import EXAMS_SFTP_SETTINGS
+from exams.pearson import (
+    download,
+)
+from exams.pearson.sftp_test import (
+    EXAMS_SFTP_SETTINGS,
+    EXAMS_SFTP_RESULTS_DIR,
+)
 
 
 @override_settings(**EXAMS_SFTP_SETTINGS)
@@ -24,3 +33,22 @@ class PeasonUploadTest(SimpleTestCase):
 
         assert local_path == expected_local_path
         sftp.get.assert_called_once_with(remote_path, localpath=expected_local_path)
+
+    def test_filtered_files(self):  # pylint: disable=no-self-use
+        """
+        Test that filtered_files filters on the regex
+        """
+        listdir_values = ['a.ext', 'b.ext', 'b']
+        isfile_values = [True, False, True]
+        sftp = Mock()
+        sftp.listdir.return_value = listdir_values
+        sftp.isfile.side_effect = isfile_values
+
+        pattern = re.compile(r'\S+\.ext')
+
+        result = list(download.filtered_files(sftp, pattern))
+
+        assert result == ['a.ext']
+
+        sftp.listdir.assert_called_once_with(EXAMS_SFTP_RESULTS_DIR)
+        assert sftp.isfile.call_args_list == [call(arg) for arg in listdir_values]
