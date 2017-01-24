77983f1ef37f1758753f31a68f5a843489116479
Nathan Levesque <nlevesq@mit.edu>
2017-01-24 09:56:14 -0500

    Working sftp filtered processing

--- exams/pearson/download.py
+++ exams/pearson/download.py
@@ -38,7 +38,7 @@ def filtered_files(sftp, pattern):
         sftp (pysftp.Connection): the connection to fetch the file from
         pattern (re): the regex pattern to mathc the filename on
     """
-    for remote_path in sftp.listdir(settings.EXAMS_SFTP_RESULTS_DIR):
+    for remote_path in sftp.listdir():
         if sftp.isfile(remote_path) and pattern.match(remote_path):
             yield remote_path
 
@@ -47,21 +47,40 @@ def process_result_files(pattern, process_result):
     """
     Processes result files on fromt he sftp server
 
+
+    Examples:
+
+        # process all vcdc files and print each line
+        import re
+
+        pattern = re.compile('vcdc-.*')
+
+        def process(file):
+            with open(file, 'r') as f:
+                for line in f.read().splitlines()
+                    print(line)
+            return True
+
+        process_result_files(pattern, process)
+
     Args:
         pattern (regex): regex to match the filename against
-        process_result(callable): callable to invoke per-file for processing
+        process_result(callable): callable to invoke per-file for processing, returns True to remove the file from the remote
     """
     try:
         with get_connection() as sftp:
-            for remote_path in filtered_files(sftp, pattern):
-                local_path = fetch_file(sftp, remote_path)
+            with sftp.cd(settings.EXAMS_SFTP_RESULTS_DIR):
+                for remote_path in filtered_files(sftp, pattern):
+                    local_path = fetch_file(sftp, remote_path)
 
-                try:
-                    if process_result(local_path):
-                        sftp.remove(remote_path)
-                    log.debug("Processed remote file: %s", remote_path)
-                except:  # pylint: disable=bare-except
-                    log.exception("Error processing file: %s", remote_path)
+                    log.debug('Fetching result file: %s', remote_path)
+
+                    try:
+                        if process_result(local_path):
+                            sftp.remove(remote_path)
+                        log.debug("Processed remote file: %s", remote_path)
+                    except:  # pylint: disable=bare-except
+                        log.exception("Error processing file: %s", remote_path)
 
     except SSHException as ex:
         raise RetryableSFTPException() from ex
