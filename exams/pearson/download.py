4e9581ceda4245a53fe87d7d81d5b3db190c62cd
Nathan Levesque <nlevesq@mit.edu>
2017-01-25 17:33:03 -0500

    First pass on handling zip files

--- exams/pearson/download.py
+++ exams/pearson/download.py
@@ -1,5 +1,6 @@
 """Pearson SFTP download implementation"""
 import logging
+import re
 from os import path
 
 from django.conf import settings
@@ -9,80 +10,137 @@ from exams.pearson.exceptions import RetryableSFTPException
 from exams.pearson.sftp import get_connection
 
 
+ZIP_FILE_RE = re.compile(r'.+\.zip')
+EXTRACTED_FILE_RE = re.compile(r"""
+    ^
+    (                        # supported file types
+        vcdc |               # Vue Candidate Data Confirmation
+        eac                  # Exam Authorization Confirmation
+    )
+    \-
+    (\d{4}\-\d{2}\-\d{2})    # date of file export
+    .*?                      # nothing standard after the date
+    \.dat                    # extension
+    $
+""", re.VERBOSE)
+
 log = logging.getLogger(__name__)
 
 
-def fetch_file(sftp, remote_path):
+class ArchivedResponseProcesser(object):
     """
-    Fetches a remote file and returns the local path
-
-    Args:
-        sftp (pysftp.Connection): the connection to fetch the file from
-        remote_path (str): the remote path of the file to fetch
-
-    Returns:
-        local_path (str): the local path of the file
+    Handles fetching and processing of files stored in a ZIP archive on Pearson SFTP
     """
-    local_path = path.join(settings.EXAMS_SFTP_TEMP_DIR, remote_path)
+    def __init__(self, sftp):
+        self.sftp = sftp
 
-    sftp.get(remote_path, localpath=local_path)
+    def fetch_file(self, remote_path):
+        """
+        Fetches a remote file and returns the local path
 
-    return local_path
+        Args:
+            remote_path (str): the remote path of the file to fetch
 
+        Returns:
+            local_path (str): the local path of the file
+        """
+        local_path = path.join(settings.EXAMS_SFTP_TEMP_DIR, remote_path)
 
-def filtered_files(sftp, pattern):
-    """
-    Walks a directory and yeilds files that match the pattern
+        sftp.get(remote_path, localpath=local_path)
 
-    Args:
-        sftp (pysftp.Connection): the connection to fetch the file from
-        pattern (re): the regex pattern to mathc the filename on
-    """
-    for remote_path in sftp.listdir():
-        if sftp.isfile(remote_path) and pattern.match(remote_path):
-            yield remote_path
+        return local_path
 
+    def filtered_files(self):
+        """
+        Walks a directory and yeilds files that match the pattern
 
-def process_result_files(pattern, process_result):
-    """
-    Processes result files on fromt he sftp server
+        Args:
+            pattern (re): the regex pattern to mathc the filename on
+        """
+        for remote_path in sftp.listdir():
+            if self.sftp.isfile(remote_path) and self.file_pattern.match(remote_path):
+                yield remote_path, self.fetch_file(remote_path)
 
+    def process(self):
+        """Process response files"""
+        with sftp.cd(settings.EXAMS_SFTP_RESULTS_DIR):
+            for remote_path, local_path in self.filtered_files():
+                try:
+                    self.process_zip(local_path)
+                    log.debug("Processed remote file: %s", remote_path)
 
-    Examples:
+                    self.sftp.remove(remote_path)
 
-        # process all vcdc files and print each line
-        import re
+                except SSHException:
+                    raise
+                except:  # pylint: disable=bare-except
+                    log.exception("Error processing file: %s", remote_path)
+                    os.remove(local_path)
 
-        pattern = re.compile('vcdc-.*')
+        except SSHException as ex:
+            raise RetryableSFTPException() from ex
 
-        def process(file):
-            with open(file, 'r') as f:
-                for line in f.read().splitlines()
-                    print(line)
-            return True
+    def process_zip(self, local_path):
+        """
+        Process a single zip file
 
-        process_result_files(pattern, process)
+        Args:
+            local_path (str): path to the zip file on the local filesystem
+        """
+        processed = True
 
-    Args:
-        pattern (regex): regex to match the filename against
-        process_result(callable): callable to invoke per-file for processing
-            return True to signal that the file should be removed from the remote
-    """
-    try:
-        with get_connection() as sftp:
-            with sftp.cd(settings.EXAMS_SFTP_RESULTS_DIR):
-                for remote_path in filtered_files(sftp, pattern):
-                    local_path = fetch_file(sftp, remote_path)
+        # extract the zip and walk the files
+        with ZipFile(local_path) as zipfile:
+            for extracted_filename in zipfile.namelist():
+                with zip_file.open(extracted_filename) as extracted_file:
+                    processed = process and self.process_extracted_file(extracted_file)
 
-                    log.debug('Fetching result file: %s', remote_path)
+        return processed
 
-                    try:
-                        if process_result(local_path):
-                            sftp.remove(remote_path)
-                            log.debug("Removed remote file: %s", remote_path)
-                        log.debug("Processed remote file: %s", remote_path)
-                    except:  # pylint: disable=bare-except
-                        log.exception("Error processing file: %s", remote_path)
+    def process_extracted_file(self, extracted_file):
+        """
+        Processes an individual file extracted from the zip
 
-    except SSHException as ex:
-        raise RetryableSFTPException() from ex
+        Args:
+            extraced_file (zipfile.ZipExtFile): the extracted file-like object
+        """
+
+        match = EXTRACTED_FILE_RE.match(extracted_file.name)
+
+        if not match:
+            return False # we don't support this type of file
+
+        file_type = match.group(0)
+
+        if file_type == 'vcdc':
+            # We send Pearson CDD files and get the results as VCDC files
+            return self.process_vcdc_file(extracted_file)
+        elif file_type == 'eac'
+            # We send Pearson EAD files and get the results as EAC files
+            return self.process_eac_file(extracted_file)
+
+        return False
+
+    def process_vcdc_file(self, extracted_file):
+        """
+        Processes a VCDC file extracted from the zip
+
+        Args:
+            extraced_file (zipfile.ZipExtFile): the extracted file-like object
+
+        Returns:
+            (bool): flag for successful processing of the file
+        """
+        return False
+
+    def process_eac_file(self, extracted_file):
+        """
+        Processes a EAC file extracted from the zip
+
+        Args:
+            extraced_file (zipfile.ZipExtFile): the extracted file-like object
+
+        Returns:
+            (bool): flag for successful processing of the file
+        """
+        return False
