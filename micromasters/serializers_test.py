b9bca7cc2422af0beef2491af6119d5e879be2df
David Baumgold <david@davidbaumgold.com>
2017-02-01 11:10:27 -0500

    Add test docstrings

--- micromasters/serializers_test.py
+++ micromasters/serializers_test.py
@@ -17,6 +17,9 @@ class UserTests(MockedESTestCase):
     Tests for serializing users.
     """
     def test_basic_user(self):
+        """
+        Test serializing a basic user, no profile
+        """
         with mute_signals(post_save):
             user = UserFactory.create(email="fake@example.com")
 
@@ -30,6 +33,9 @@ class UserTests(MockedESTestCase):
         }
 
     def test_logged_in_user_through_maybe_wrapper(self):
+        """
+        Test serialize_maybe_user
+        """
         with mute_signals(post_save):
             user = UserFactory.create(email="fake@example.com")
 
@@ -43,6 +49,9 @@ class UserTests(MockedESTestCase):
         }
 
     def test_user_with_profile(self):
+        """
+        Test a user with a profile
+        """
         with mute_signals(post_save):
             user = UserFactory.create(email="fake@example.com")
             ProfileFactory.create(
@@ -63,6 +72,9 @@ class UserTests(MockedESTestCase):
 
     @mock.patch('micromasters.serializers.get_social_username')
     def test_social_username(self, mock_get_username):
+        """
+        Make sure serializer gets social username
+        """
         mock_get_username.return_value = "remote"
         with mute_signals(post_save):
             user = UserFactory.create(
@@ -85,6 +97,9 @@ class AnonymousUserTests(TestCase):
     Tests for serializing anonymous users.
     """
     def test_serialize_anonymous_user(self):
+        """
+        Test serializing an anonymous user
+        """
         anon_user = AnonymousUser()
         data = serialize_maybe_user(anon_user)
         assert data is None
