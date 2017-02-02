e3f6e66b539703b4c4986477999c538b7cd396ec
George Schneeloch <gschneel@mit.edu>
2017-02-02 17:49:55 -0500

    Don't check for /?coupon, add test for presence in context

--- cms/util.py
+++ cms/util.py
@@ -5,14 +5,23 @@ from urllib.parse import urlparse, parse_qs
 
 
 def get_coupon_code(request):
-    coupon_code = request.GET.get("coupon", None)
-    if coupon_code:
-        return coupon_code
-    next_url = request.GET.get("next", None)
+    """
+    Get coupon code from an HttpRequest
+
+    Args:
+        request (django.http.request.HttpRequest): An HttpRequest
+
+    Returns:
+        str: A coupon code or None if none found
+    """
+    next_url = request.GET.get("next")
     if not next_url:
         return None
     parsed = urlparse(next_url)
-    coupons = parse_qs(parsed.query).get("coupon", None)
+    path = parsed.path
+    if path not in ("/dashboard", "/dashboard/"):
+        return None
+    coupons = parse_qs(parsed.query).get("coupon")
     if coupons:
         return coupons[0]
     return None
--- cms/util_test.py
+++ cms/util_test.py
@@ -1,9 +1,10 @@
 """
 Tests for cms/util.py
 """
-from micromasters.test import SimpleTestCase
 from django.http.request import HttpRequest
+
 from cms.util import get_coupon_code
+from micromasters.test import SimpleTestCase
 
 
 class UtilTestCase(SimpleTestCase):
@@ -14,14 +15,6 @@ class UtilTestCase(SimpleTestCase):
         super().setUp()
         self.request = HttpRequest()
 
-    def test_coupon_in_query(self):
-        """
-        Test if the coupon is directly in the query string
-        """
-        self.request.GET["coupon"] = "abc"
-        coupon = get_coupon_code(self.request)
-        assert coupon == "abc"
-
     def test_coupon_in_next_url(self):
         """
         Test if the coupon is in the query string of the `next` url
@@ -29,6 +22,14 @@ class UtilTestCase(SimpleTestCase):
         self.request.GET["next"] = "/dashboard?coupon=abc"
         coupon = get_coupon_code(self.request)
         assert coupon == "abc"
+
+    def test_coupon_in_wrong_next_url(self):
+        """
+        We should ignore a coupon code that's not for the dashboard
+        """
+        self.request.GET["next"] = "/profile?coupon=abc"
+        coupon = get_coupon_code(self.request)
+        assert coupon is None
 
     def test_no_coupon(self):
         """
