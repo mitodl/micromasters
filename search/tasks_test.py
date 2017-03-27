000a6ce81f2a1f1a2f8fda37f8286d49ec15ca83
George Schneeloch <gschneel@mit.edu>
2017-03-27 09:46:19 -0400

    Add refresh_index to fix race condition with percolate

--- search/tasks_test.py
+++ search/tasks_test.py
@@ -1,5 +1,6 @@
 """Tests for search tasks"""
 from dashboard.factories import ProgramEnrollmentFactory
+from django.test import override_settings
 from search.base import MockedESTestCase
 from search.tasks import (
     index_users,
@@ -7,6 +8,10 @@ from search.tasks import (
 )
 
 
+FAKE_INDEX = 'fake'
+
+
+@override_settings(ELASTICSEARCH_INDEX=FAKE_INDEX)
 class SearchTasksTests(MockedESTestCase):
     """
     Tests for search tasks
@@ -29,17 +34,23 @@ class SearchTasksTests(MockedESTestCase):
         """
         enrollment1 = ProgramEnrollmentFactory.create()
         enrollment2 = ProgramEnrollmentFactory.create(user=enrollment1.user)
-        index_users([enrollment1.user])
+        with self.patch('search.tasks.refresh_index', autospec=True) as refresh_index_mock:
+            index_users([enrollment1.user])
         self.index_users_mock.assert_called_with([enrollment1.user])
         for enrollment in [enrollment1, enrollment2]:
             self.send_automatic_emails_mock.assert_any_call(enrollment)
+        assert refresh_index_mock.call_count == 1
+        refresh_index_mock.assert_called_with(FAKE_INDEX)
 
     def test_index_program_enrolled_users(self):
         """
         When we run the index_program_enrolled_users task we should index them and send them automatic emails
         """
         enrollments = [ProgramEnrollmentFactory.create() for _ in range(2)]
-        index_program_enrolled_users(enrollments)
+        with self.patch('search.tasks.refresh_index', autospec=True) as refresh_index_mock:
+            index_program_enrolled_users(enrollments)
         self.index_program_enrolled_users_mock.assert_called_with(enrollments)
         for enrollment in enrollments:
             self.send_automatic_emails_mock.assert_any_call(enrollment)
+        assert refresh_index_mock.call_count == 1
+        refresh_index_mock.assert_called_with(FAKE_INDEX)
