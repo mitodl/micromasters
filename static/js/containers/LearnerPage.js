5a27e6a4d247bde4bca3946a0bf5326b72a75184
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2017-04-19 19:26:51 +0500

    Updated titles

--- static/js/containers/LearnerPage.js
+++ static/js/containers/LearnerPage.js
@@ -39,7 +39,7 @@ class LearnerPage extends React.Component<*, LearnerPageProps, *> {
     const { params: { username }, fetchProfile } = this.props;
     fetchProfile(username);
     this.fetchDashboard();
-    document.title = "Learner's Profile";
+    document.title = "My Profile";
   }
 
   componentDidUpdate() {
