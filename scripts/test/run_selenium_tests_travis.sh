afbdca3b1225a5233f4f0e5ef2090c2237a070ed
George Schneeloch <gschneel@mit.edu>
2017-07-10 11:18:49 -0400

    Lower log level for selenium scripts for easier debugging

--- scripts/test/run_selenium_tests_travis.sh
+++ scripts/test/run_selenium_tests_travis.sh
@@ -8,8 +8,10 @@ then
     echo "webpack-stats.json must exist before running the selenium tests. Run webpack to create it."
     exit 1
 fi
-docker-compose run \
+docker-compose -f travis-docker-compose.yml run \
    -e DEBUG=False \
    -e DJANGO_LIVE_TEST_SERVER_ADDRESS=0.0.0.0:7000-8000 \
    -e ELASTICSEARCH_INDEX=testindex \
+   -e MICROMASTERS_LOG_LEVEL=DEBUG \
+   -e DJANGO_LOG_LEVEL=DEBUG \
    selenium py.test ./selenium_tests
