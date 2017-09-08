176303df72c7fdbd018f1a0e7287766c6b493cb8
George Schneeloch <gschneel@mit.edu>
2017-09-08 18:35:55 -0400

    Tests

--- discussions/urls.py
+++ discussions/urls.py
@@ -12,5 +12,5 @@ from discussions.views import (
 urlpatterns = [
     url(r'^api/v0/discussions_token/$', discussions_token, name='discussions_token'),
     url(r'^discussions/$', discussions_redirect, name='discussions'),
-    url(r'^api/v0/channels/$', ChannelsView.as_view(), name='channels-list'),
+    url(r'^api/v0/channels/$', ChannelsView.as_view(), name='channel-list'),
 ]
