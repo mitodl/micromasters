5921228e336d3cbc2aaedf9e454ca319ef375776
George Schneeloch <gschneel@mit.edu>
2017-10-17 18:01:05 -0400

    Revert "Swap order"
    This reverts commit 0a3909acafa0e56274cedb1913c480474866ff9b.

--- scripts/test/run_selenium_tests_travis.sh
+++ scripts/test/run_selenium_tests_travis.sh
@@ -8,5 +8,5 @@ then
     echo "webpack-stats.json must exist before running the selenium tests. Run webpack to create it."
     exit 1
 fi
-docker-compose -f docker-compose.selenium.yml -f docker-compose.travis.yml -f docker-compose.yml run \
+docker-compose -f docker-compose.yml -f docker-compose.travis.yml -f docker-compose.selenium.yml run \
    selenium py.test ./selenium_tests
