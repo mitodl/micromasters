0fe2be955e6b6b51909a8e3f4f07ec613414c655
annagav <annagav@mit.edu>
2018-03-20 13:52:10 -0400

    too-many-locals

--- selenium_tests/management/commands/snapshot_dashboard_states.py
+++ selenium_tests/management/commands/snapshot_dashboard_states.py
@@ -74,7 +74,7 @@ def bind_args(func, *args, **kwargs):
     return lambda: func(*args, **kwargs)
 
 
-class DashboardStates:
+class DashboardStates:  # pylint: disable=too-many-locals
     """Runs through each dashboard state taking a snapshot"""
     def __init__(self, user=None):
         """
