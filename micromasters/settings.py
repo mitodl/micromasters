36ef1554fb1e0a6c5dcc2c8801be4f29ca939600
George Schneeloch <gschneel@mit.edu>
2018-06-11 14:50:29 -0400

    Remove

--- micromasters/settings.py
+++ micromasters/settings.py
@@ -371,7 +371,7 @@ LOGGING = {
         'django': {
             'propagate': True,
             'level': DJANGO_LOG_LEVEL,
-            'handlers': ['console', 'syslog', 'sentry'],
+            'handlers': ['console', 'syslog'],
         },
         'django.request': {
             'handlers': ['mail_admins'],
@@ -397,7 +397,7 @@ LOGGING = {
         },
     },
     'root': {
-        'handlers': ['console', 'syslog', 'sentry'],
+        'handlers': ['console', 'syslog'],
         'level': LOG_LEVEL,
     },
 }
