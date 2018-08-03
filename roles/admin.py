a468aa4a4106d10aa7941d47487782641a21c38b
Peter Pinch <pdpinch@mit.edu>
2018-08-03 09:06:30 -0400

    oops. didn't mean to remove user from the list display

--- roles/admin.py
+++ roles/admin.py
@@ -9,7 +9,7 @@ from roles.models import Role
 
 class RoleAdmin(admin.ModelAdmin):
     """ModelAdmin for Roles"""
-    list_display = ('program', 'role', )
+    list_display = ('user', 'program', 'role', )
     raw_id_fields = ('user', )
 
 admin.site.register(Role, RoleAdmin)
