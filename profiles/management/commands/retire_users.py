6d99990b8ec5efa1c5d43e93d823323d5c168508
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2018-10-24 19:03:50 +0500

    added logs

--- profiles/management/commands/retire_users.py
+++ profiles/management/commands/retire_users.py
@@ -1,12 +1,15 @@
 """
 Retire user from MM
 """
+import logging
 from argparse import RawTextHelpFormatter
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand, CommandError
 from social_django.models import UserSocialAuth
 
 from dashboard.models import ProgramEnrollment
+
+log = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -43,6 +46,17 @@ For multiple users, add arg `--user` for each user i.e:\n
             help="Single or multiple user name"
         ),
 
+    def display_messages(self, message, log_messages, is_error=False):
+        """
+        Display error on console
+        Args:
+            message (str): message to display
+            is_error (bool): is error for styling
+            log_messages (list): Accumulated message
+        """
+        self.stdout.write(message, style_func=self.style.ERROR if is_error else None)
+        log_messages.append(message)
+
     def handle(self, *args, **kwargs):  # pylint: disable=unused-argument
         user_names = kwargs.get("users", [])
 
@@ -51,40 +65,54 @@ For multiple users, add arg `--user` for each user i.e:\n
             raise CommandError("Please select user(s)")
 
         for user_name in user_names:
+            log_messages = []
+
             # retire user
-            self.stdout.write(f"Retiring user {user_name}")
+            self.display_messages(f"Retiring user {user_name}", log_messages)
 
             if not user_name:
                 # invalid user name, can be empty string
-                self.stdout.write(f"Invalid username: '{user_name}'", self.style.ERROR)
+                self.display_messages(
+                    f"Invalid username: '{user_name}'",
+                    log_messages,
+                    self.style.ERROR
+                )
                 continue
 
             try:
                 user = User.objects.get(username=user_name)
             except User.DoesNotExist:
-                self.stdout.write(
+                self.display_messages(
                     f"User '{user_name}' does not exist in MicroMasters",
-                    self.style.ERROR
+                    log_messages,
+                    is_error=True
                 )
                 continue
 
             # mark user inactive
             user.is_active = False
             user.save()
-            self.stdout.write(f"User {user_name} is_active set to False")
+            self.display_messages(f"User {user_name} is_active set to False", log_messages)
 
             # reset email_optin
             user.profile.email_optin = False
             user.profile.save()
-            self.stdout.write(f"User {user_name} email_optin set to False")
+            self.display_messages(f"User {user_name} email_optin set to False", log_messages)
 
             # reset program enrollments
             enrollment_delete_count, _ = ProgramEnrollment.objects.filter(user=user).delete()
-            self.stdout.write(f"User {user_name}, {enrollment_delete_count} ProgramEnrollments rows deleted")
+            self.display_messages(
+                f"For user {user_name}: {enrollment_delete_count} ProgramEnrollments rows deleted",
+                log_messages
+            )
 
             # reset user social
             auth_delete_count, _ = UserSocialAuth.objects.filter(user=user).delete()
-            self.stdout.write(f"User {user_name}, {auth_delete_count} SocialAuth rows deleted")
+            self.display_messages(
+                f"For user {user_name}: {auth_delete_count} SocialAuth rows deleted",
+                log_messages
+            )
 
             # finish
-            self.stdout.write(f"User '{user_name}' is retired")
+            self.display_messages(f"User '{user_name}' is retired", log_messages)
+            log.info(", ".join(log_messages))
