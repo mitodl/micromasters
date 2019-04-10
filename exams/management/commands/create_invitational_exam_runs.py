bc6d34f49291f36f972d862d3861a7147f36fe77
annagav <annagav@mit.edu>
2019-04-10 16:42:34 -0400

    change arguments

--- exams/management/commands/create_invitational_exam_runs.py
+++ exams/management/commands/create_invitational_exam_runs.py
@@ -1,41 +1,36 @@
 """
-Creates expired exam runs for particular selection of students
+Creates exam authorization for particular selection of students
 """
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand, CommandError
 
-from courses.models import CourseRun
-from exams.factories import ExamRunFactory
 from exams.models import ExamRun, ExamAuthorization
 
 
 class Command(BaseCommand):
     """
-    Creates ExamAuthorization for expired ExamRun for particular course_run and user
+    Creates ExamAuthorization for given ExamRun id and username
     """
-    help = "Creates ExamAuthorization for expired ExamRun for particular course_run and user"
+    help = "Creates ExamAuthorization for provided ExamRun id and username"
 
     def add_arguments(self, parser):
-        parser.add_argument("edx_course_key", help="the edx_course_key for the course run")
-        parser.add_argument("username", help="the edx_course_key for the course run")
+        parser.add_argument("exam_run_id", help="the exam_run id created manually")
+        parser.add_argument("username", help="the username of the learner")
 
     def handle(self, *args, **kwargs):  # pylint: disable=unused-argument
-        edx_course_key = kwargs.get('edx_course_key')
+        exam_run_id = kwargs.get('exam_run_id')
         username = kwargs.get('username')
 
         user = User.objects.get(username=username)
         if user is None:
             raise CommandError('Username {} does not exist'.format(username))
         try:
-            run = CourseRun.objects.get(edx_course_key=edx_course_key)
-        except CourseRun.DoesNotExist:
-            raise CommandError('Course Run for course_id "{}" does not exist'.format(edx_course_key))
+            exam_run = ExamRun.objects.get(id=exam_run_id)
+        except ExamRun.DoesNotExist:
+            raise CommandError('ExamRun with this id "{}" does not exist'.format(exam_run_id))
 
-        exam_run = ExamRun.get_schedulable_in_past(run.course).order_by('-date_last_schedulable').first()
-        if exam_run is None:
-            exam_run = ExamRunFactory.create(course=run.course, scheduling_past=True, eligibility_past=True)
         ExamAuthorization.objects.get_or_create(
             user=user,
-            course=run.course,
+            course=exam_run.course,
             exam_run=exam_run,
         )
