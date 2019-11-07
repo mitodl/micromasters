c3da506c8cf761450d97960441f23a0aceb314d8
tasawernawaz <tasawer.nawaz@arbisoft.com>
2019-11-07 15:13:28 +0500

    acknowlege course team on program page

---
+++ cms/blocks.py
@@ -0,0 +1,14 @@
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class CourseTeamBlock(blocks.StructBlock):
+    """
+    Block class that defines a course team member
+    """
+
+    name = blocks.CharBlock(max_length=100, help_text="Name of the course team member.")
+    bio = blocks.CharBlock(max_length=255, help_text="Short bio of course team member.")
+    image = ImageChooserBlock(
+        help_text='Image for the faculty member. Should be 385px by 385px.'
+    )
