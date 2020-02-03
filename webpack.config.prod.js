6d33b787ac82134a6c7f187faa2640cc45e41181
annagav <annagav@mit.edu>
2020-02-02 23:48:26 -0500

    install mini-css-extract-plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 var path = require("path");
 var BundleTracker = require('webpack-bundle-tracker');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const { config, babelSharedLoader } = require(path.resolve("./webpack.config.shared.js"));
 
 const prodBabelConfig = Object.assign({}, babelSharedLoader);
@@ -17,10 +17,14 @@ prodConfig.module.rules = [
   ...config.module.rules,
   {
     test: /\.css$|\.scss$/,
-    use:     ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use:      ['css-loader', 'postcss-loader', 'sass-loader'],
-    })
+    use:  [
+      {
+        loader: MiniCssExtractPlugin.loader
+      },
+      "css-loader",
+      "postcss-loader",
+      "sass-loader"
+    ]
   }
 ];
 
@@ -46,11 +50,9 @@ module.exports = Object.assign(prodConfig, {
       minimize: true
     }),
     new webpack.optimize.AggressiveMergingPlugin(),
-    new ExtractTextPlugin({
-      filename: "styles-[name]-[contenthash].css",
-      allChunks: true,
-      ignoreOrder: false,
-    }),
+    new MiniCssExtractPlugin({
+      filename: "styles-[name]-[contenthash].css"
+    })
   ],
   optimization: {
     minimize: true
