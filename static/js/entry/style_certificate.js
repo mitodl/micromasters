77568920c1f0c4231f40d2c0f4c5426214de5984
HamzaIbnFarooq <hamza.farooq@arbisoft.com>
2021-02-25 16:13:58 +0500

    service certificate related css separately

---
+++ static/js/entry/style_certificate.js
@@ -0,0 +1,4 @@
+__webpack_public_path__ = `${SETTINGS.public_path}` // eslint-disable-line no-undef, camelcase
+
+// The file having all the scss regarding certificates
+import "../../scss/certificate.scss"
