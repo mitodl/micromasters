576770553a156f9962ee59581985a69b76461240
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2021-06-22 17:26:12 +0500

    Run recreate index through a celery task

--- search/management/commands/recreate_index.py
+++ search/management/commands/recreate_index.py
@@ -3,6 +3,10 @@ Management command to recreate the Elasticsearch index
 """
 
 from django.core.management.base import BaseCommand
+
+from search.indexing_api import (
+    __name__ as indexing_api_name,
+)
 from search.tasks import recreate_index_async
 
 
@@ -16,5 +20,21 @@ class Command(BaseCommand):
         """
         Recreates the index
         """
-        recreate_index_task = recreate_index_async.delay()
-        self.stdout.write('Running reindexing task with Id: {}'.format(recreate_index_task.id))
+        log = logging.getLogger(indexing_api_name)
+        console = logging.StreamHandler(self.stderr)
+        console.setLevel(logging.DEBUG)
+        log.addHandler(console)
+        log.level = logging.INFO
+
+        if kwargs['profile']:
+            import cProfile
+            import uuid
+            profile = cProfile.Profile()
+            profile.enable()
+            recreate_index_async.delay()
+            profile.disable()
+            filename = 'recreate_index_{}.profile'.format(uuid.uuid4())
+            profile.dump_stats(filename)
+            self.stdout.write('Output profiling data to: {}'.format(filename))
+        else:
+            recreate_index_async.delay()
