cf2d29974a2c24c030c5723588c82b9df05c59a2
Nathan Levesque <rhysyngsun@gmail.com>
2021-10-18 12:23:00 -0400

    Allow at most 1 social auth per provider (#5094)

--- micromasters/settings.py
+++ micromasters/settings.py
@@ -179,6 +179,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.social_auth.associate_by_email',
+    'backends.pipeline_api.limit_one_auth_per_backend',
     'backends.pipeline_api.check_edx_verified_email',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
