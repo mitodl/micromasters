2b1df8ac7c4395cc0a0227d936b3f021f0ae3019
Asad Iqbal <7334669+asadiqbal08@users.noreply.github.com>
2021-10-26 10:56:35 +0500

    Added possible fix for target container is not a DOM element (#5098)
    * Added possible fix for target container is not a DOM element
    
    * linting
    
    Co-authored-by: asadiqbal08 <asad.iqbal@arbisoft.com>

--- static/js/entry/dashboard.js
+++ static/js/entry/dashboard.js
@@ -43,7 +43,9 @@ const renderApp = Component => {
   )
 }
 
-renderApp(DashboardRouter)
+if (rootEl) {
+  renderApp(DashboardRouter)
+}
 
 if (module.hot) {
   module.hot.accept("../DashboardRouter", () => {
