1297491f347c20cbb56ea5da780c60b5a6c0c403
Arslan <arslan.ashraf@arbisoft.com>
2022-10-11 16:28:52 +0500

    refactor: recator the enroll button for MITxOnline (DEDP)

--- ecommerce/constants.py
+++ ecommerce/constants.py
@@ -1,4 +1,6 @@
 """Ecommerce constants"""
+from urllib.parse import urljoin
+from django.conf import settings
 
 # From secure acceptance documentation, under API reply fields:
 # http://apps.cybersource.com/library/documentation/dev_guides/Secure_Acceptance_SOP/Secure_Acceptance_SOP.pdf
@@ -9,3 +11,5 @@ CYBERSOURCE_DECISION_ERROR = 'ERROR'
 CYBERSOURCE_DECISION_CANCEL = 'CANCEL'
 
 REFERENCE_NUMBER_PREFIX = 'MM-'
+
+MITXONLINE_CART_URL = urljoin(settings.MITXONLINE_URL, "/cart/")
