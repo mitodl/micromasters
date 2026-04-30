e89c407c7606306829cac0a20368fe2a358f5bb5
annagav <annagav@users.noreply.github.com>
2026-04-30 12:55:36 -0400

    List Final Grades with edx_course_key (#5510)
    * List Final Grades with edx_course_key
    
    * doc string
    
    * comment

--- grades/admin.py
+++ grades/admin.py
@@ -10,13 +10,19 @@ from micromasters.utils import get_field_names
 class FinalGradeAdmin(admin.ModelAdmin):
     """Admin for FinalGrade"""
     model = models.FinalGrade
-    list_display = ('id', 'grade', 'user', 'course_run', )
+    list_display = ('id', 'grade', 'user', 'get_edx_course_key', 'course_run',)
     list_filter = ('course_run',)
     ordering = ('course_run',)
     raw_id_fields = ('user',)
     search_fields = (
         'user__username',
     )
+
+    def get_edx_course_key(self, obj):
+        """Return the edX course key for the related course run."""
+        return obj.course_run.edx_course_key
+
+    get_edx_course_key.short_description = 'Edx Course Key'
 
     def has_delete_permission(self, *args, **kwargs):  # pylint: disable=unused-argument, signature-differs
         return False
